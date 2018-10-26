d90518612fb0dac8e25ff12f07b6c7fed1abaf9e
Jcamacho53 <jonathoncamacho319@gmail.com>
2018-10-26 10:52:31 -0400

    Add files via upload
    minor inprovements

---
+++ databaseListener.php
@@ -0,0 +1,97 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+$serverName="localhost";
+$username="username";
+$password="password";
+
+//Connect to Database
+$conn=mysqli_connect($severname, $username, $password, $IT490);
+
+if ($conn) {
+die ("Connection failed:") .(mysqli_connect_error());
+}
+echo"Connection successful";
+
+if (mysqli_connect_error())
+{
+	echo "Failed to connect to MySQL:".mysql_connect_error();
+	exit();
+}
+print "Successfully connect to MySql.<br><br><br>";
+
+//submit query to Database
+function doLogin($username,$password)
+{
+   
+$sql="INSERT INTO users (username, password) Values ('username', 'password')";
+
+($t = msqli_query ($db, $sql)) or die (mysql_error($db));
+
+$num = mysql_num_rows($t);
+
+//$s="SELECT  * FROM users WHERE ";
+//echo"<br>$s<br>";
+//($t=mysquli_query($db, $s)) or die (mysqli_error($db));
+//$num = mysql_num_rows($t);
+
+if ($num >0)
+
+{return true;} 
+
+else
+{return false;}
+
+}
+
+//Validate login credentials match
+
+$Username = POST_['username'];
+$Password = POST_['password'];
+
+$select1="SELECT FROM users WHERE username=$Username AND password=$Password";
+($t=mysquli_query($db, $select1)) or die (mysqli_error($db));
+$num = mysql_num_rows($t);
+
+if ($num >0) 
+
+{
+ return true;
+ echo "Login Successful";
+}
+else
+{
+ return false;
+ echo "Login Failed";
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+mysqli_free_result($t);
+mysqli_close($db);
+exit("<br>Interaction completed.<br><br>");
+mysquli_close($IT490);
+?>

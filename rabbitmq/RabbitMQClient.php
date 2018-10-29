d3697046e2e64437e5b2927ae930473de29d70b6
pmalazarte <43541197+pmalazarte@users.noreply.github.com>
2018-10-29 09:44:33 -0400

    Update RabbitMQClient.php

--- rabbitmq/RabbitMQClient.php
+++ rabbitmq/RabbitMQClient.php
@@ -3,6 +3,9 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+
+///used for login and register files
+function doLogin($email, $password){
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
@@ -14,12 +17,40 @@ else
 }
 $request = array();
 $request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
+$request['email'] = "$email";
+$request['password'] = "$password";
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
+
 echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
 echo $argv[0]." END".PHP_EOL;
+}
+
+function doRegister($firstname, $lastname, $email, $password){
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "Register";
+}
+$request = array();
+$request['type'] = "Register";
+$request['firstname'] = "$firstname";
+$request['lastname'] = "$lastname";
+$request['email'] = "$email";
+$request['password'] = "$password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+echo $argv[0]." END".PHP_EOL;	
+}

61f5ece2b07883125869d9379cdd86f2a4e3b31b
Jcamacho53 <jonathoncamacho319@gmail.com>
2018-11-01 20:33:04 -0400

    Add files via upload
    listener that detects errors and sends an error log to a file

---
+++ errorListener.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+//show errors
+display_errors = on
+error_reporting = E_ALL
+//log errors
+log_errors = on
+error_log = "/tmp/php-error.log"
+error_log("An error has occured",3,error.log);
+?>

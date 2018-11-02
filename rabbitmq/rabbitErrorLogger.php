8b348165b3f7b4b8f31920a1d36b53b5476b4a01
dogeofsb <43014554+dogeofsb@users.noreply.github.com>
2018-11-01 23:46:24 -0400

    Delete rabbitErrorLogger.php

--- rabbitmq/rabbitErrorLogger.php
+++
@@ -1,8 +0,0 @@
-#! /usr/lib/rabbitmq/lib/rabbitmq_server-3.5.7/ebin/rabbit_error_logger.beam
-#! /usr/lib/rabbitmq/lib/rabbitmq_server-3.5.7/ebin/rabbit_error_logger_file_h.beam
-<?php 
-
-echo "(cat  rabbit_error_logger.beam")
-echo "(cat rabbit_error_logger_file_h.beam")
-
-
